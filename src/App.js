e10ce412655b4b3116fb44f72f5b9cb2e1c908ba
Ryan Jandrick B. Obeles <51547772+rjbobeles@users.noreply.github.com>
2020-11-09 13:24:35 +0800

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { SubmissionProvider } from './hook/useSubmission'
+
+import ClosedView from './view/Closed'
+import ErrorView from './view/Error'
+import HomeView from './view/Home'
+import SoonView from './view/Soon'
+import ThankYouView from './view/ThankYou'
+import NotFoundView from './view/404'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <HomeView/>} />
+      <Route exact path="/soon" render={() => <SoonView/>} />
+      <Route exact path="/closed" render={() => <ClosedView/>} />
+      <Route exact path="/thankyou" render={() => <ThankYouView/>} />
+      <Route exact path="/error" render={() => <ErrorView/>} />
+      <Route path="*" render={() => <NotFoundView/>} />
+    </Switch>
   );
 }
 
-export default App;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => (
+  <SubmissionProvider>
+    <Router>
+      <App { ...props }/>
+    </Router>
+  </SubmissionProvider>
+)
