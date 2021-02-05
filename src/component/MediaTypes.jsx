9d0534cb6c1817370797bd545d6c926068d05f9e
Gel Gamoras <gelgamoras@gmail.com>
2021-02-05 08:03:51 +0800

    added animation

--- src/component/MediaTypes.jsx
+++ src/component/MediaTypes.jsx
@@ -1,21 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { motion } from 'framer-motion'
+import { IntersectionContext } from './IntersectionObserver'
 import PropTypes from 'prop-types'
 
 MediaTypes.propTypes = {
+  i: PropTypes.number,
   title: PropTypes.string,
   description: PropTypes.string,
   description2: PropTypes.string,
 }
 
 export default function MediaTypes(props) {
+  const { inView } = useContext(IntersectionContext)
+
+  const variants = {
+    hidden: {
+      y: '20px',
+      opacity: 0,
+      transition: {
+        type: 'spring',
+        damping: 40,
+      },
+    },
+    show: {
+      y: '0',
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        damping: 40,
+      },
+    },
+  }
+
   return (
-    <div className="entry flex flex-row items-stretch mb-8">
-      <div className="photo border border-smoke flex-shrink-0"></div>
-      <div className="text pl-4 sm:pl-8 flex flex-col justify-center lg:justify-start xl:justify-center text-roast">
+    <motion.div
+      className="entry flex flex-row items-stretch mb-12 sm:mb-16"
+      initial="hidden"
+      animate={inView ? 'show' : 'hidden'}
+      transition={{ type: 'spring', damping: 20 }}
+      variants={variants}
+    >
+      <div className={`photo flex-shrink-0 ${props.illus}`}></div>
+      <div className="text pl-6 sm:pl-12 flex flex-col justify-center lg:justify-start xl:justify-center text-roast">
         <h5 className="text-2xl xl:text-3xl mb-2">{props.title}</h5>
-        <p className="text-xs sm:text-base">{props.description}</p>
-        <p className="text-xs sm:text-base mt-5">{props.description2}</p>
+        <p className="text-sm sm:text-base">{props.description}</p>
+        <p className="text-sm sm:text-base mt-5">{props.description2}</p>
       </div>
-    </div>
+    </motion.div>
   )
 }
--- src/component/PhotoText.jsx
+++ src/component/PhotoText.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { motion } from 'framer-motion'
+import { IntersectionContext } from './IntersectionObserver'
 import PropTypes from 'prop-types'
 
 PhotoText.propTypes = {
@@ -7,16 +9,41 @@ PhotoText.propTypes = {
 }
 
 export default function PhotoText(props) {
+  const { inView } = useContext(IntersectionContext)
+
+  const variants = {
+    hidden: {
+      y: '40px',
+      opacity: 0,
+    },
+    show: {
+      y: '0',
+      opacity: 1,
+    },
+  }
+
   return (
     <div className="wrapper mx-auto flex flex-col md:flex-row items-center md:items-start py-8 md:py-16">
-      <div className="w-full sm:w-1/2 lg:w-7/12 flex flex-row justify-end md:pr-5">
-        <div className="w-full photo bg-smoke mb-5 md:mb-0"></div>
+      <div className="w-full sm:w-1/2 lg:w-1/2 flex flex-row justify-end md:pr-5">
+        <motion.div
+          className={`w-full photo mb-8 md:mb-0 ${props.illus}`}
+          initial="hidden"
+          animate={inView ? 'show' : 'hidden'}
+          transition={{ type: 'spring', damping: 40 }}
+          variants={variants}
+        ></motion.div>
       </div>
-      <div className="w-full sm:w-1/2 lg:w-5/12 md:pl-5">
-        <div className="w-full text-roast">
+      <div className="w-full sm:w-1/2 lg:w-1/2 md:pl-12">
+        <motion.div
+          className="w-full text-roast"
+          initial="hidden"
+          animate={inView ? 'show' : 'hidden'}
+          transition={{ type: 'spring', damping: 40, delay: 0.4 }}
+          variants={variants}
+        >
           <p className="mb-8">{props.texta}</p>
           <p>{props.textb}</p>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
